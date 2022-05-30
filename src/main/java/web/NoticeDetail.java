d5379d95322c7788dd56f379722c911686e57d5c
SeungJun <ttws0107@gmail.com>
2022-05-30 10:30:35 +0900

    Notice Prevent from viewCount add

--- src/main/java/web/NoticeDetail.java
+++ src/main/java/web/NoticeDetail.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,24 +22,43 @@ public class NoticeDetail extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		NoticeDAO dao = new NoticeDAO();
+		int n_no = Integer.parseInt(request.getParameter("n_no"));
+		
+		Cookie[] cookies = request.getCookies();
+		Cookie isCookie = null;
+		
+		if (cookies != null && cookies.length > 0) { // 사용자가 가진 쿠키 중 해당 게시글에 해당하는 쿠키가 있다면 iscookie에 저장
+			for (int i = 0; i < cookies.length; i++) {
+				if (cookies[i].getName().equals("cookieN"+n_no))
+					isCookie = cookies[i];
+			}
+		}
+		if (isCookie == null) { // 해당게시글 쿠키가 없으면 쿠키 등록
+			Cookie newCookie = new Cookie("cookieN" + n_no, "|" + n_no + "|");
+			response.addCookie(newCookie);
+			dao.countUp(n_no); //등록 후 게시글 조회수 증가
+		} else {
+			//있을경우니까 아무일도 일어나지 않지만 테스트로 쿠키값을 띄워보자
+			System.out.println(isCookie.getValue());
+		}
+		
 		if(request.getParameter("n_no") != null
 				&& util.Util.str2Int(request.getParameter("n_no"))) {
-			int n_no = Integer.parseInt(request.getParameter("n_no"));
 			
 			RequestDispatcher rd = request.getRequestDispatcher("./noticeDetail.jsp");
-			
-			NoticeDAO dao = new NoticeDAO();
 			NoticeDTO dto = dao.noticeDetail(n_no);
 			System.out.println(dto.getResultcount());
 			if(dto.getResultcount() == 1) {
 				request.setAttribute("dto", dto);
-				rd.forward(request, response);
+				rd.forward(request, response);				
 			} else {
 				response.sendRedirect("./main");
 			}
-		} else {
+		} else {			
 			response.sendRedirect("./main");
 		}
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
